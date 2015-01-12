0a98c2b0900326516c584abfe1468faa7ed9a679
Motaz <Motaz.Elshaer@badrit.com>
2015-01-13 01:22:09 +0200

    finishing all formmating

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 *
 * Description
 */
-var savingsInterestApp = angular.module('savingsInterestApp', ['fiestah.money']); 
+var savingsInterestApp = angular.module('savingsInterestApp', [ 'ng-currency', 'fiestah.money']); 
 
 savingsInterestApp.controller('calculatorCtr', function($scope){
 
@@ -175,24 +175,5 @@ savingsInterestApp.controller('calculatorCtr', function($scope){
 
 	/*** first call ***/ 
 	$scope.changeEvent();
-
-
-	$scope.formatCurrancy  =function (amount) {
-		var i = parseFloat(amount);
-		if(isNaN(i)) { i = 0.00; }
-		var minus = '';
-		if(i < 0) { minus = '-'; }
-		i = Math.abs(i);
-		i = parseInt((i + .005) * 100);
-		i = i / 100;
-		s = new String(i);
-		if(s.indexOf('.') < 0) { s += '.00'; }
-		if(s.indexOf('.') == (s.length - 2)) { s += '0'; }
-
-		////////////         here we should put our code            /////////////////
-		s = minus + s;
-		return s;
-	}
-
 });
 
